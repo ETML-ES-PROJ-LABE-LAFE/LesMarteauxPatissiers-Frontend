d1c13ad732425bda0d6abc6ba74b4b58e2ef5ece
BrunoSilvPinto <bruno.silva@eduvaud.ch>
2024-05-24 14:01:24 +0200

    feat: create function to add lot with button + subcategories

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -2,23 +2,42 @@ import axios from 'axios';
 const API_URL = "http://localhost:8080/api/categories";
 
 class CategoryService {
-  async getCategory() {
-    try {
-      const response = await axios.get(API_URL);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Erreur HTTP ${error.response.status}: ${error.response.data}`);
-    }
-  }
+	async getCategory() {
+		try {
+			const response = await axios.get(API_URL);
+			return response.data;
+		} catch (error) {
+			throw new Error(
+				`Erreur HTTP ${error.response.status}: ${error.response.data}`
+			);
+		}
+	}
 
-  async getSubCategories(categoryId) {
-    try {
-      const response = await axios.get(`${API_URL}/${categoryId}/subcategories`);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Erreur HTTP ${error.response.status}: ${error.response.data}`);
-    }
-  }
+	async getParentCategories() {
+		try {
+			const categories = await this.getCategory();
+			console.log(categories);
+			return categories.filter(category => category.parentCategoryId === null);
+		} catch (error) {
+			throw new Error(
+				`Erreur lors de la récupération des catégories parentes: ${error.message}`
+			);
+		}
+	}
+
+	async getSubCategories(categoryId) {
+		try {
+			const response = await axios.get(
+				`${API_URL}/${categoryId}/subcategories`
+			);
+			return response.data;
+		} catch (error) {
+			throw new Error(
+				`Erreur HTTP ${error.response.status}: ${error.response.data}`
+			);
+		}
+	}
 }
 
 export default new CategoryService();
+
--- src/views/AjoutItems.vue
+++ src/views/AjoutItems.vue
@@ -1,146 +1,159 @@
 <template>
-    <div class="ajout-items">
-      <h1>Ajout d'un nouveau lot</h1>
-      <form @submit.prevent="addItem">
-        <div class="form-group">
-          <label for="name">Nom:</label>
-          <input type="text" id="name" v-model="name" required>
-        </div>
-        <div class="form-group">
-          <label for="category">Catégorie:</label>
-          <select id="category" v-model="categoryId" required>
-            <option disabled value="">Sélectionnez une catégorie</option>
-            <option v-for="cat in categories" :key="cat.id" :value="cat.id">{{ cat.name }}</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="subCategory">Sous-Catégorie:</label>
-          <select id="subCategory" v-model="subCategoryId" required>
-            <option disabled value="">Sélectionnez une sous-catégorie</option>
-            <option v-for="subCat in subCategories" :key="subCat.id" :value="subCat.id">{{ subCat.name }}</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="description">Description:</label>
-          <input type="text" id="description" v-model="description" required>
-        </div>
-        <div class="form-group">
-          <label for="initialPrice">Prix initial:</label>
-          <input type="number" id="initialPrice" v-model="initialPrice" required>
-        </div>
-        <div class="form-group">
-          <button type="submit">Ajouter</button>
-        </div>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import itemService from '@/services/itemService.js';
-  import categoryService from '@/services/CategoryService.js';
-  
-  export default {
-    name: 'AjoutItems',
-    data() {
-      return {
-        name: '',
-        categoryId: '',
-        subCategoryId: '',
-        description: '',
-        initialPrice: 0,
-        categories: [],
-        subCategories: []
+  <div class="ajout-items">
+    <h1>Ajout d'un nouveau lot</h1>
+    <form @submit.prevent="addItem">
+      <div class="form-group">
+        <label for="name">Nom:</label>
+        <input type="text" id="name" v-model="name" required>
+      </div>
+      <div class="form-group">
+        <label for="category">Catégorie:</label>
+        <select id="category" v-model="categoryId" required>
+          <option disabled value="">Sélectionnez une catégorie</option>
+          <option v-for="cat in parentCategories" :key="cat.id" :value="cat.id">{{ cat.name }}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="subCategory">Sous-Catégorie:</label>
+        <select id="subCategory" v-model="subCategoryId" required>
+          <option disabled value="">Sélectionnez une sous-catégorie</option>
+          <option v-for="subCat in subCategories" :key="subCat.id" :value="subCat.id">{{ subCat.name }}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="description">Description:</label>
+        <input type="text" id="description" v-model="description" required>
+      </div>
+      <div class="form-group">
+        <label for="initialPrice">Prix initial:</label>
+        <input type="number" id="initialPrice" v-model="initialPrice" required>
+      </div>
+      <div class="form-group">
+        <button type="submit">Ajouter</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import itemService from '@/services/itemService.js';
+import categoryService from '@/services/CategoryService.js';
+
+export default {
+  name: 'AjoutItems',
+  data() {
+    return {
+      name: '',
+      categoryId: '',
+      subCategoryId: 0,
+      description: '',
+      initialPrice: 0,
+      parentCategories: [],
+      subCategories: []
+    };
+  },
+  methods: {
+    async addItem() {
+      // Vérification des valeurs de categoryId et subCategoryId
+      if (!this.categoryId || !this.subCategoryId) {
+        console.error('Catégorie ou sous-catégorie non sélectionnée');
+        return;
+      }
+
+      const newItem = {
+        name: this.name,
+        categoryId: this.subCategoryId,
+        description: this.description,
+        initialPrice: this.initialPrice
       };
-    },
-    methods: {
-      async addItem() {
-        const newItem = {
-          name: this.name,
-          category: { id: this.categoryId },
-          subCategory: { id: this.subCategoryId },
-          description: this.description,
-          initialPrice: this.initialPrice
-        };
-  
-        console.log('Tentative d\'ajout de l\'item:', newItem); // Debug: log de l'item à ajouter
-  
-        try {
-          await itemService.addItem(newItem);
-          console.log('Item ajouté avec succès'); // Debug: confirmation d'ajout
-          this.showSuccessAlert();
-          this.$router.push('/');
-        } catch (error) {
-          console.error('Erreur lors de l\'ajout de l\'item:', error);
-        }
-      },
-      async fetchCategories() {
-        try {
-          const categories = await categoryService.getCategory();
-          this.categories = categories;
-        } catch (error) {
-          console.error('Erreur lors de la récupération des catégories:', error);
-        }
-      },
-      showSuccessAlert() {
-        alert("L'item a été ajouté avec succès!");
+
+      try {
+        await itemService.addItem(newItem);
+        console.log('Item ajouté avec succès'); // Debug: confirmation d'ajout
+        this.showSuccessAlert();
+        this.$router.push('/');
+      } catch (error) {
+        console.error('Erreur lors de l\'ajout de l\'item:', error);
       }
     },
-    watch: {
-      categoryId(newCategoryId) {
-        const selectedCategory = this.categories.find(cat => cat.id === newCategoryId);
-        this.subCategories = selectedCategory ? selectedCategory.subCategories : [];
+    async fetchParentCategories() {
+      try {
+        const parentCategories = await categoryService.getParentCategories();
+        this.parentCategories = parentCategories;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des catégories parentes:', error);
       }
     },
-    created() {
-      this.fetchCategories();
+    async fetchSubCategories(categoryId) {
+      try {
+        const subCategories = await categoryService.getSubCategories(categoryId);
+        this.subCategories = subCategories;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des sous-catégories:', error);
+      }
+    },
+    showSuccessAlert() {
+      alert("L'item a été ajouté avec succès!");
     }
-  };
-  </script>
-  
-  <style scoped>
-  .ajout-items {
-    max-width: 600px;
-    margin: 50px auto 0; /* Ajout d'une marge en haut de 50px */
-    padding: 20px;
-    background-color: #333; /* Fond noir correspondant à la barre de menu */
-    color: #fff; /* Texte blanc */
-    border-radius: 8px;
+  },
+  watch: {
+    categoryId(newCategoryId) {
+      if (newCategoryId) {
+        this.fetchSubCategories(newCategoryId);
+      } else {
+        this.subCategories = [];
+      }
+    }
+  },
+  created() {
+    this.fetchParentCategories();
   }
-  .ajout-items h1 {
-    text-align: center;
-    margin-bottom: 20px;
-    color: #42b983; /* Vert pour le titre */
-  }
-  .form-group {
-    margin-bottom: 20px;
-  }
-  .form-group label {
-    display: block;
-    margin-bottom: 5px;
-    color: #42b983; /* Vert pour les labels */
-  }
-  .form-group input,
-  .form-group select {
-    width: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    border-radius: 4px;
-    border: 1px solid #ccc;
-    background-color: #444; /* Fond gris foncé pour les champs */
-    color: #fff; /* Texte blanc pour les champs */
-  }
-  .form-group button {
-    display: block;
-    width: 100%;
-    padding: 10px;
-    background-color: #42b983; /* Fond vert pour le bouton */
-    color: #000; /* Texte noir pour le bouton */
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  .form-group button:hover {
-    background-color: #36a572; /* Vert plus foncé au survol */
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.ajout-items {
+  max-width: 600px;
+  margin: 50px auto 0;
+  padding: 20px;
+  background-color: #333;
+  color: #fff;
+  border-radius: 8px;
+}
+.ajout-items h1 {
+  text-align: center;
+  margin-bottom: 20px;
+  color: #42b983;
+}
+.form-group {
+  margin-bottom: 20px;
+}
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+  color: #42b983;
+}
+.form-group input,
+.form-group select {
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background-color: #444;
+  color: #fff;
+}
+.form-group button {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  background-color: #42b983;
+  color: #000;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.form-group button:hover {
+  background-color: #36a572;
+}
+</style>
